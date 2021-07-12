980091dda65f57896b1eb95b76da7e464bef53bd
naru200 <jinsu2504@gmail.com>
2021-07-12 18:02:44 +0900

    Init project

---
+++ android/app/src/main/java/com/naru200rnboilerplate/MainActivity.java
@@ -0,0 +1,15 @@
+package com.naru200rnboilerplate;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "Naru200RNBoilerplate";
+  }
+}
