31c5a7f1eb76a7b880fa9c913fc32535f2dd8269
naru200 <jinsu2504@gmail.com>
2021-09-30 19:35:24 +0900

    Setup Eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+  ],
+  env: {
+    node: true,
+  },
+  rules: {
+    "react/jsx-closing-bracket-location": ['warn', 'line-aligned'],
+    "object-curly-spacing": ['warn', 'always'],
+    "comma-dangle": ['warn', "always-multiline"],
+    "arrow-parens": ["warn", "as-needed"],
+  },
 };
