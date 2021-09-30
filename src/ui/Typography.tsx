c2cfcdc5ebd21ffaac1a3efc0ccf28f65c2ac6b2
naru200 <jinsu2504@gmail.com>
2021-09-30 18:55:30 +0900

    Add ui/Typograhpy

---
+++ src/ui/Typography.tsx
@@ -0,0 +1,36 @@
+import { Factory, Text } from 'native-base';
+
+export const Text10 = Factory(Text, {
+  baseStyle: {
+    color: 'black',
+    fontSize: 'xxs',
+  },
+});
+
+export const Text12 = Factory(Text, {
+  baseStyle: {
+    color: 'black',
+    fontSize: 'xs',
+  },
+});
+
+export const Text14 = Factory(Text, {
+  baseStyle: {
+    color: 'black',
+    fontSize: 'sm',
+  },
+});
+
+export const Text16 = Factory(Text, {
+  baseStyle: {
+    color: 'black',
+    fontSize: 'md',
+  },
+});
+
+export const Text18 = Factory(Text, {
+  baseStyle: {
+    color: 'black',
+    fontSize: 'lg',
+  },
+});
